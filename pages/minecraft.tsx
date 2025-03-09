c8ba1ad0e4bab163e8e255882a41ea61fe16f717
Myster7494 <jackychiu0101@gmail.com>
2025-03-09 16:44:44 +0800

    fix

--- pages/minecraft.tsx
+++ pages/minecraft.tsx
@@ -7,14 +7,15 @@ export default function Minecraft() {
         <meta property="og:title" content="Minecraft Online"/>
         <meta property="twitter:title" content="Minecraft Online"/>
         <meta property="og:description"
-              content="Play Minecraft Online.Share this link to friends and play together!"/>
+              content="Play Minecraft Online. Share this link to friends and play together!"/>
         <meta property="twitter:description"
               content="Play Minecraft Online.Share this link to friends and play together!"/>
         <meta name="og:image" content="https://minecraft.wiki/images/MC_key_art_2024.jpg"/>
+        <meta name="twitter:image" content="https://minecraft.wiki/images/MC_key_art_2024.jpg"/>
         <meta property="og:image:alt"
               content="Play Minecraft Online.Share this link to friends and play together!"/>
-        <meta property="og:image:width" content="1920"/>
-        <meta property="og:image:height" content="1080"/>
+        <meta property="og:image:width" content="3840"/>
+        <meta property="og:image:height" content="2160"/>
         <meta property="og:url" content="https://myster7494.github.io/minecraft"/>
         <meta name="twitter:card" content="summary_large_image"/>
         <meta http-equiv="refresh" content="0;url=https://youtu.be/aCgP8BFjrw4"/>
